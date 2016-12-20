61cc1625cbda6e199f552fffdae8a3d97f44d6e9
Vic Lam <lam.vic.k@gmail.com>
2016-12-20 01:50:14 -0500

    Destroy all recipes!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,51 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
+  # Index Action
   get '/' do
     erb :index
   end
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  # New Action
+  get '/recipes/new' do
+    erb :new
+  end
+  post '/recipes' do
+    attributes = {name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time]}
+    @recipe = Recipe.create(attributes)
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  # Edit Action
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+  patch '/recipes/:id' do
+    attributes = {name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time]}
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.update(attributes)
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  # Delete Action
+  delete '/recipes/:id/delete' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.destroy
+    redirect to '/recipes'
+  end
+
+  # Show Action
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+
 end
